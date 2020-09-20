1ea6e2bcd728ff7103e812d4eb5a2216a16d1399
zhangtao <zhangtao87868@163.com>
2020-09-21 06:05:35 +0800

    feat(*): setState

--- main.js
+++ main.js
@@ -3,10 +3,20 @@
 // }
 import { createElement, Component, render } from './toy-react'
 class MyComponent extends Component {
+  constructor() {
+    super()
+    this.state = {
+      a: 1,
+      b: 2
+    }
+  }
   render() {
     return <div>
       <h1>my component</h1>
       {this.children}
+      <button onclick={() => { this.setState({ a: this.state.a + 1 }) }}>add</button>
+      <span>{this.state.a.toString()}</span>
+      <span>{this.state.b.toString()}</span>
     </div>
   }
 }
